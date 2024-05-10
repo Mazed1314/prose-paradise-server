b68a92bb09d1736ebf9bfecd524c98e46f0f79ee
Mazed1413 <mamazed1314@gmail.com>
2024-05-10 17:18:45 +0600

    get single data from blog

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://prose-paradise.web.app",
+      "https://prose-paradise.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.Db_pass}@cluster0.tvtcgta.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -31,6 +40,12 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogCollection.findOne(query);
+      res.send(result);
+    });
     app.post("/addBlog", async (req, res) => {
       const addNewBlog = req.body;
       //   console.log(addNewBlog);
@@ -39,7 +54,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
